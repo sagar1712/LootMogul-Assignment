d6e6aaeae726837d5c4292298ea9ca323c4ffd1c
sagar-blok <sagar@blokument.com>
2024-07-16 23:42:21 +0530

    feat: implement hero section

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,17 @@
+const HeroSection = () => {
+    return (
+      <div className="flex flex-col justify-center text-center text-white">
+        <h1 className="pt-16 text-6xl">
+          LootMogul External API <br />
+          Integration
+        </h1>
+        <p className="pt-8 text-xl pb-16">
+          Bring blockchain to the people. Solana supports experiences <br />
+          for power users, new consumers, and everyone in between.
+        </p>
+      </div>
+    );
+  };
+  
+  export default HeroSection;
+  
